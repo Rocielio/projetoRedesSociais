1178582ad2a37d73c1d65116e27c8200e19596a2
Rocielio <109254358+Rocielio@users.noreply.github.com>
2022-09-12 21:45:49 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let imagemIframe = document.querySelector("iframe#telaCelular")
+
+function chamarPagFacebook() {
+    imagemIframe.src = "./redesSociais/telaFacebook.html"
+}
+
+function chamarPagGithub() {
+    imagemIframe.src = "./redesSociais/telaGithub.html"
+}
+
+function chamarPagInstagram() {
+    imagemIframe.src = "./redesSociais/telaInstagram.html"
+}
+
+function chamarPagTwitter() {
+    imagemIframe.src = "./redesSociais/telaTwitter.html"
+}
+
+function chamarPagYoutube() {
+    imagemIframe.src = "./redesSociais/telaYoutube.html"
+}
+
+function chamarHome() {
+    imagemIframe.src = "./redesSociais/telaHome.html"
+}
